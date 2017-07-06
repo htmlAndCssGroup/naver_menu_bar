38fd4b31ceac42fe771d6f27479dd414f022cffe
moonformeli <mgyang95@gmail.com>
2017-07-06 12:18:03 +0900

    moverover - mouseleave menu bar

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/*
 $(document).ready(function(){
   $('.more').click(function(){
     if($('.more').hasClass('more')){
@@ -8,4 +9,32 @@ $(document).ready(function(){
        $('.board').css('visibility', 'hidden');
     }
   });
+});
+*/
+
+$(document).ready(function(){
+	$('.more').mouseover(function(){
+		if($('.more').hasClass('more')){
+			$('.more').addClass('close').removeClass('more');
+			$('.board').css('visibility', 'visible');
+		}
+	});
+	$('.more').mouseleave(function(){
+		if($('.close').hasClass('close')){
+			$('.close').addClass('more').removeClass('close');
+			$('.board').css('visibility', 'hidden');
+		}
+	});
+	$('.board').mouseover(function(){
+		if($('.more').hasClass('more')){
+			$('.more').addClass('close').removeClass('more');
+			$('.board').css('visibility', 'visible');
+		}
+	});
+	$('.board').mouseleave(function(){
+		if($('.close').hasClass('close')){
+			$('.close').addClass('more').removeClass('close');
+			$('.board').css('visibility', 'hidden');
+		}
+	});
 });
